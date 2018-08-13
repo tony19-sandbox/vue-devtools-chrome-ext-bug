48fd1712e4ecda2bf78eafac2848252fe811fc99
Tony Trinh <tony19@gmail.com>
2018-08-13 13:31:44 -0500

    Add vue-devtools

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,12 @@
+import devtools from '@vue/devtools';
 import Vue from 'vue';
 import App from './App';
 import store from '../store';
 import router from './router';
+
+if (process.env.NODE_ENV === 'development') {
+  devtools.connect();
+}
 
 /* eslint-disable no-new */
 new Vue({
